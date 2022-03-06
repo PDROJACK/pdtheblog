4b80fa802dc624efe0d65925e27f5631df33820b
pdrojack <singhpd75@gmail.com>
2022-03-06 21:27:40 +0530

    changed info

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
+    siteTitle: `PD's Blog`,
+    // siteTitleAlt: `Minimal Blog - Gatsby Theme`,
+    siteHeadline: `PD's Blog`,
     siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
+    siteDescription: `Personl blog of priyadarshan singh aka pd aka pdrojack`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@pdrojack`,
   },
   plugins: [
     {
@@ -33,12 +33,12 @@ module.exports = {
         ],
         externalLinks: [
           {
-            name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            name: `Github`,
+            url: `https://github.com/PDROJACK`,
           },
           {
-            name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
+            name: `Spotify`,
+            url: `https://open.spotify.com/user/7ozvre5g7h6seahj9npmpdjqj?si=b443a3c8cd0c4298`,
           },
         ],
       },
